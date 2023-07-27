e81c6a4736ee1485381ee3f65ad2fecaa3598390
Crowdbotics <team@crowdbotics.com>
2023-07-27 06:21:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-hill-42460",
+  url: "https://aged-hill-42460.botics.co",
+  api: "https://aged-hill-42460.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
